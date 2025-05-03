5772a3d10c48d9d91ffdacf5a18e4f11264cd79e
Shadab Mohammad <39692236+shadabshaukat@users.noreply.github.com>
2025-05-03 17:23:28 +1000

    Update multiple_port_forward.sh

--- multiple_port_forward.sh
+++ multiple_port_forward.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CONFIG_FILE="vm_services.json"
-BASTION_ID="ocid1.bastion.oc1.ap-melbourne-1.amaaaaaawiclygaazrbmmbzo6dop2w7yyrd2lhmlpx6aecvfg7n64srizpoa"
-PUBLIC_KEY_FILE="/Users/shadab/Downloads/shadablenovo.pub"
-PRIVATE_KEY_FILE="/Users/shadab/Downloads/mydemo_vcn.priv"
-PROFILE="apaccpt04"
+BASTION_ID="ocid1.bastion.oc1.ap-melbourne-1.axxxxxxxxxxxxxx"
+PUBLIC_KEY_FILE="/Users/shadab/Downloads/xxxxx.pub"
+PRIVATE_KEY_FILE="/Users/shadab/Downloads/xxxxx.priv"
+PROFILE="apxxxxxxx"
 STARTING_LOCAL_PORT=2222
 
 oci setup repair-file-permissions --file $PRIVATE_KEY_FILE
